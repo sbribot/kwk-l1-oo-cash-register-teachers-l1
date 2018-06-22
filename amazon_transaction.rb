e58bd080b518d50a13c81da4f9129ee538d20e38
Sara Beth Ribot <sbribot@gmail.com>
2018-06-22 20:35:14 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -4,12 +4,14 @@ class AmazonTransaction
   attr_accessor :subtotal, :discount, :items
   
   def initialize
-    total = 0
-    discount = 0 
-    items = []
+    @total = 0
+    @discount = 0 
+    @items = []
   end
   
-  
+  def add_item(name,price,number)
+    
+  end
 
 
 
